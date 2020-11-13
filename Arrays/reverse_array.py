5d24c10baea79a05ee54fe211d64ab563a20dbcf
Mayank Gupta <g.mayank82@yahoo.com>
2020-11-13 12:55:35 +0530

    Reverse array iterative way

--- Arrays/reverse_array.py
+++ Arrays/reverse_array.py
@@ -1,10 +1,17 @@
+# Iterative solution for reversing an array
+
+# Function to reverse Arrar(Arr) from start to end(length of Arr)
 def reverse_arr(Arr, start, end):
     while start < end:
         Arr[start], Arr[end] = Arr[end], Arr[start]
         start += 1
         end -= 1
 
+# Driver function for the above function
 Arr = [1,2,3,4,5]
 print(Arr)
+# Calling the function created above and passing in values
+# Initializing start and end indexes as start = 0 and end = n - 1
 reverse_arr(Arr, 0, len(Arr)-1)
+# Printing the reversed array
 print("Reversed : ", Arr)
